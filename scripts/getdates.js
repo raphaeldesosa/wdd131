19aac9c5cf5362c01c5835b0e2d16d7b5cdabeeb
raphaeldesosa <des22005@byui.edu>
2024-06-17 22:29:20 +0800

    Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentyear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentyear;
+
+let lastModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = lastModified
