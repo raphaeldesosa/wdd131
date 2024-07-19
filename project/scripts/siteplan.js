9778b40d443f07e7d7fb0dc9f8614754e26cdb8a
raphaeldesosa <des22005@byui.edu>
2024-07-19 14:54:23 +0800

    Site plan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+let currentyear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentyear;
+
+let lastModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = lastModified
+    
